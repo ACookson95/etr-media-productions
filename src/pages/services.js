c61ff837ef305bebffee3855374fd89ad1a8c32e
Aaron Cookson <57735633+ACookson95@users.noreply.github.com>
2022-10-27 22:36:09 -0600

    Adding styled components to service

--- src/pages/services.js
+++ src/pages/services.js
@@ -5,15 +5,29 @@ import { Row, Col, Container } from "react-bootstrap";
 import { StaticImage } from "gatsby-plugin-image";
 import styled from "styled-components";
 
-const Service = styled.div`
+const ServiceItem = styled(Row)``;
+
+const ServiceItemCol = styled(Col)`
   display: flex;
-  height: 300px;
+  flex-direction: column;
+  justify-content: space-evenly;
+  background-color: ${props => props.bg && props.theme.colors[props.bg] + "95"};
+  color: ${props => props.theme.colors[props.color] && props.theme.colors[props.color]};
+`;
 
-  .description {
-    flex: 1 1 0%;
-    min-width: 400px;
+const ServiceItemTitle = styled.h3``;
+
+const ServiceItemSubtitle = styled.h4``;
+
+const ServiceItemText = styled.p``;
+
+const ServiceItemImgWrap = styled(Col)`
+  margin-left: 0;
+  z-index: -1;
+  @media (max-width: ${props => props.theme.breakpoints.sm}) {
+    margin-left: -100%;
   }
-
+  padding: 0;
 `;
 
 const Services = () => {
@@ -31,71 +45,65 @@ const Services = () => {
               </p>
             </Col>
           </Row>
-        </Container>
-        <Service as={Container}>
-          <div className="description bg-secondary bg-opacity-25 p-5">
-            <h4>Marketing Strategy</h4>
-            <h5>Product Launches, Social Media, Websites</h5>
-            <p>
-              Every business should have a clear plan of action in place to
-              guide the company towards their desired goals. By asking clear and
-              targeted questions, we are able to help create a unique plan
-              specific for your company.
-            </p>
-          </div>
-          <StaticImage
-            src="../images/strategy.jpg"
-            alt="strategy"
-            style={{
-              height: "100%",
-              width: "45%",
-              minWidth: "300px",
-              inset: 0,
-              objectFit: "cover",
-              flex: "0 0 auto",
-              zIndex: 0
-            }}
-          />
-        </Service>
-        <Container>
-          <Row className="g-0 bg-dark text-light">
-            <Col xs={5}>
+          <ServiceItem>
+            <ServiceItemCol xs={12} lg={7} bg="light2">
+              <ServiceItemTitle>Marketing Strategy</ServiceItemTitle>
+              <ServiceItemSubtitle>
+                Product Launches, Social Media, Websites
+              </ServiceItemSubtitle>
+              <ServiceItemText>
+                Every business should have a clear plan of action in place to
+                guide the company towards their desired goals. By asking clear
+                and targeted questions, we are able to help create a unique plan
+                specific for your company.
+              </ServiceItemText>
+            </ServiceItemCol>
+            <ServiceItemImgWrap>
               <StaticImage
-                src="../images/branding.jpg"
-                alt="branding products"
-                style={{
-                  height: "100%",
-                  width: "100%",
-                  inset: 0,
-                  objectFit: "cover",
-                }}
+                src="../images/strategy.jpg"
+                alt="strategy"
               />
-            </Col>
-            <Col className="p-3 mx-3 my-auto">
-              <h4>Brand Identity</h4>
-              <h5>Logos, Packaging, Brand Colors</h5>
-              <p>
+            </ServiceItemImgWrap>
+          </ServiceItem>
+          <ServiceItem className="flex-row-reverse">
+            <ServiceItemCol xs={12} lg={7}>
+              <ServiceItemTitle>Brand Identity</ServiceItemTitle>
+              <ServiceItemSubtitle>
+                Logos, Packaging, Brand Colors
+              </ServiceItemSubtitle>
+              <ServiceItemText>
                 Each brand is unique. We embrace that uniqueness by creating
                 logos, marketing videos, and custom packaging specific to your
                 needs. We'll help ensure that your brand and products will be
                 clearly represented to all of your clients.
-              </p>
-            </Col>
-          </Row>
-          <Row className="g-0 bg-light">
-            <Col className="p-3 mx-3 my-auto">
-              <h4>Digital Media</h4>
-              <h5>Videography, Photography, Social Media Posts</h5>
-              <p>
+              </ServiceItemText>
+            </ServiceItemCol>
+            <ServiceItemImgWrap>
+              <StaticImage
+                src="../images/branding.jpg"
+                alt="strategy"
+              />
+            </ServiceItemImgWrap>
+          </ServiceItem>
+          <ServiceItem>
+            <ServiceItemCol xs={12} lg={7} bg="dark1" color="light1">
+              <ServiceItemTitle>Digital Media</ServiceItemTitle>
+              <ServiceItemSubtitle>
+                Videography, Photography, Social Media Posts
+              </ServiceItemSubtitle>
+              <ServiceItemText>
                 We help to deliver top tier quality media to your clients in a
                 variety of formats. We create tailored content to help you reach
                 the customers you want and need.
-              </p>
-            </Col>
-            <Col xs={5}>
-              <StaticImage src="../images/media.jpeg" alt="filming" />
-            </Col>
-          </Row>
+              </ServiceItemText>
+            </ServiceItemCol>
+            <ServiceItemImgWrap>
+              <StaticImage
+                src="../images/media.jpeg"
+                alt="strategy"
+              />
+            </ServiceItemImgWrap>
+          </ServiceItem>
         </Container>
       </Layout>
     </>
