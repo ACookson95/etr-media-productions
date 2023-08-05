57c53ca76f696f801cb40a8b536f2840ac0cdbf1
Aaron Cookson <57735633+ACookson95@users.noreply.github.com>
2023-08-05 14:40:59 -0600

    Add portfolio .md

---
+++ src/content/pageData/portfolio.md
@@ -0,0 +1,22 @@
+---
+path: /portfolio
+videos:
+  - title: Real Estate
+    videoUrl: https://www.youtube.com/embed/PSnP_MqTx8c?modestbranding=1&rel=0
+    description: description
+  - title: Real Estate
+    videoUrl: https://www.youtube.com/embed/PSnP_MqTx8c?modestbranding=1&rel=0
+    description: description
+  - title: Real Estate
+    videoUrl: https://www.youtube.com/embed/PSnP_MqTx8c?modestbranding=1&rel=0
+    description: description
+  - title: Real Estate
+    videoUrl: https://www.youtube.com/embed/PSnP_MqTx8c?modestbranding=1&rel=0
+    description: description
+  - title: Real Estate
+    videoUrl: https://www.youtube.com/embed/PSnP_MqTx8c?modestbranding=1&rel=0
+    description: description
+  - title: Real Estate
+    videoUrl: https://www.youtube.com/embed/PSnP_MqTx8c?modestbranding=1&rel=0
+    description: description
+---
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,96 +1,37 @@
 import * as React from "react";
 import Layout from "../components/Layout";
 import { SEO } from "../components/SEO";
-import VideoGrid from "../components/VideoGrid";
-import YoutubeVideo from "../components/YoutubeVideo";
-import styled from "styled-components";
+import { graphql } from "graphql";
 
-const Wrapper = styled.div`
-  background-color: black;
-  width: 100%;
-  height: 100%;
-`;
-
-const Portfolio = () => {
-  const [active, setActive] = React.useState(0);
-  const videos = [
-    {
-      id: "koA0U11i354",
-      thumbnailNum: "0",
-      title: "The Forge",
-      description: "",
-    },
-    {
-      id: "PSnP_MqTx8c",
-      thumbnailNum: "0",
-      title: "Real Estate",
-      description: "",
-    },
-    {
-      id: "vHNas6udfzk",
-      thumbnailNum: "0",
-      title: "4 Principles that Drive Us",
-      description: "",
-    },
-    {
-      id: "X1bdJdo5odo",
-      thumbnailNum: "0",
-      title: "Wangen Pump",
-      description: "",
-    },
-    {
-      id: "C1Y71QzN8Z0",
-      thumbnailNum: "0",
-      title: "Berserker",
-      description: "",
-    },
-    {
-      id: "QS_juJ4g5ZU",
-      thumbnailNum: "0",
-      title: "Chhlat Health",
-      description: "",
-    },
-    {
-      id: "M2nYmpQ-FfU",
-      thumbnailNum: "0",
-      title: "Fall Colors",
-      description: "",
-    },
-    {
-      id: "zLosZHWnyoE",
-      thumbnailNum: "0",
-      title: "A Day in the Life of a Skateboarder",
-      description: "",
-    },
-    {
-      id: "rOHlsOLVK0c",
-      thumbnailNum: "0",
-      title: "Custom Guitar Maker",
-      description: "",
-    },
-  ];
-
+const Portfolio = ({ data }) => {
+  console.log(data)
+  const { videos } = data.markdownRemark.frontmatter;
   return (
-    <>
-      <Layout>
-        <Wrapper>
-          <div>
-            <div>
-              <div></div>
-              <div>
-                <YoutubeVideo
-                  id={videos[active].id}
-                  title={videos[active].title}
-                />
-              </div>
-            </div>
-            <VideoGrid videos={videos} current={active} setActive={setActive} />
+    <Layout>
+        {videos.map((video) => (
+          <div key={video.title}>
+            <span>{video.title}</span>
+            <span>{video.description}</span>
+            <span>{video.videoUrl}</span>
           </div>
-        </Wrapper>
-      </Layout>
-    </>
-  );
+        ))}
+    </Layout>
+  )
 };
+
+export const query = graphql`
+  query PageQuery{
+    markdownRemark(frontmatter: {path: {eq: "/portfolio"}}) {
+      frontmatter {
+        videos {
+          description
+          title
+          videoUrl
+        }
+      }
+    }
+  }
+`
 
 export default Portfolio;
 
